aba7d30d1d3c46c3f7420e3188ded0a6c3a3ec30
yoonsuhchung <lucy1013@snu.ac.kr>
2022-12-26 05:58:30 +0900

    feat: signup with email verification

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path, include
+from allauth.account.views import ConfirmEmailView
+from django.urls import path, include, re_path
+from dj_rest_auth.registration.views import RegisterView, VerifyEmailView
+from dj_rest_auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view()),
+    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
+    path('account-confirm-email/',
+         VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
+            VerifyEmailView.as_view(), name='account_confirm_email'),
 ]
