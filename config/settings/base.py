fdfca18bda251377bdcd641ebb4a5bb283d27bac
seunga <cnu4530@gmail.com>
2022-12-25 14:48:18 +0900

    feat: 기본 로그인, 로그아웃, 토큰 인증 (dj-rest-auth 이용)
    dj-rest-auth에서 기본으로 제공하는 기능들로 구현하였고, token 인증
    사용하였습니다.

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,15 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CUSTOM_APPS = [
+    'api.apps.ApiConfig'
+]
+
+REST_FRAMEWORK_APPS = [
+    "rest_framework",
+    "rest_framework.authtoken",
+    "dj_rest_auth",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -48,7 +57,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-]
+] + REST_FRAMEWORK_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -120,3 +129,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
