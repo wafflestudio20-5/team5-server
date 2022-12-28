323437f0c96e1b9c4e5ccc979913167720673cb2
yoonsuhchung <lucy1013@snu.ac.kr>
2022-12-29 00:29:39 +0900

    fix: email verification bug

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,11 @@
-from allauth.account.views import ConfirmEmailView
-from django.urls import path, include, re_path
-from dj_rest_auth.registration.views import RegisterView, VerifyEmailView
-from dj_rest_auth.views import LoginView, LogoutView
+from django.urls import path, include
+from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
+from dj_rest_auth.views import LoginView
 
 urlpatterns = [
-    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
-    path('register/', RegisterView.as_view()),
+    path('dj-rest-auth/registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     path('dj-rest-auth/login/', LoginView.as_view(), name='login'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
-    path('account-confirm-email/',
-         VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
-            VerifyEmailView.as_view(), name='account_confirm_email'),
 ]
