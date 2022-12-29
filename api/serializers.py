90b441b6a2d96e419fc7527e499bf7eca24a1c70
yoonsuhchung <lucy1013@snu.ac.kr>
2022-12-30 03:34:34 +0900

    feat: custom user model/login/registration, with testcodes
    feat: custom user/login/registration
    
    fix: delete users app and mv model to api
    
    feat: testcodes for registration&login
    
    fix: remove unnecessary fields from models.py

---
+++ api/serializers.py
@@ -0,0 +1,85 @@
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
+from dj_rest_auth.serializers import LoginSerializer
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from django.contrib.auth import authenticate, get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
+from rest_framework import exceptions, serializers
+from api.models import CustomUser
+from config.settings import base
+
+
+class CustomLoginSerializer(LoginSerializer):
+    username = None
+
+    def authenticate(self, **kwargs):
+        return authenticate(self.context["request"], **kwargs)
+
+    def validate(self, attrs):
+        username = None
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = self.get_auth_user(username, email, password)
+        if not user:
+            msg = 'Unable to log in with provided credentials.'
+            raise exceptions.ValidationError(msg)
+
+        # Did we get back an active user?
+        self.validate_auth_user_status(user)
+
+        # If required, is the email verified?
+        if 'dj_rest_auth.registration' in base.INSTALLED_APPS:
+            self.validate_email_verification_status(user)
+
+        attrs['user'] = user
+        return attrs
+
+
+SIZE_CHOICES = [(220+5*i, 220+5*i)for i in range(17)]
+class CustomRegisterSerializer(RegisterSerializer):
+    username = None
+    shoe_size = serializers.ChoiceField(choices=SIZE_CHOICES)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'id',
+            'email',
+            'password',
+            'shoe_size',
+            'phone_number',
+        )
+
+    def get_cleaned_data(self):
+        return {
+            "email": self.validated_data.get("email", ""),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            "phone_number": self.validated_data.get("phone_number", ''),
+            "shoe_size": self.validated_data.get("shoe_size")
+        }
+
+    def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        setup_user_email(request, user, [])
+        user.phone_number = self.cleaned_data.get("phone_number")
+        user.shoe_size = self.cleaned_data.get("shoe_size")
+        user.save()
+        adapter.save_user(request, user, self)
+        return user
+
+
+class CustomUserDetailsSerializer(serializers.ModelSerializer):
+    phone_number = PhoneNumberField(region="KR")
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'id',
+            'email',
+            'shoe_size',
+            'phone_number',
+        )
+        read_only_fields = ('email',)
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.core import mail
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    register_url = reverse('rest_register')
+    verify_email_url = reverse('rest_verify_email')
+    login_url = reverse('login')
+
+    def test_create_account_and_login(self):
+        data = {
+            'email': 'test@example.com',
+            'password1': 'testpass@!',
+            'password2': 'testpass@!',
+            'shoe_size': '255',
+        }
+
+        response = self.client.post(self.register_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()["detail"], "Verification e-mail sent.")
+
+        login_data = {
+            'email': 'test@example.com',
+            'password': 'testpass@!'
+        }
+        response = self.client.post(self.login_url, login_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue(
+            "E-mail is not verified." in response.json()["non_field_errors"]
+        )
+
+        self.assertEqual(len(mail.outbox), 1)
+        email_lines = mail.outbox[0].body.splitlines()
+        activation_line = [i for i in email_lines if "account-confirm-email" in i][0]
+        activation_link = activation_line.split("go to ")[1]
+        activation_key = activation_link.split("/")[7]
+
+        # Verify
+        response = self.client.post(self.verify_email_url, {"key": activation_key})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()["detail"], "ok")
+
+        response = self.client.post(self.login_url, login_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("access_token" in response.json())
