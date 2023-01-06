865bbeba36896d1de950079c2975a9ee439de206
incipienstation <jinanjeong@snu.ac.kr>
2023-01-06 23:31:39 +0900

    social login 구현 (#20)
    * feat: social login draft
    
    * feat: social login
    
    * fix: accounts.views.py naver_auth
    
    * feat: social login
    
    * feat: social login finish

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 from dj_rest_auth.views import LoginView
+from .views import google_auth, naver_auth
 
 urlpatterns = [
     path('registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     path('login/', LoginView.as_view(), name='login'),
     path('', include('dj_rest_auth.urls')),
+    path('social/google/', google_auth, name='google_auth'),
+    path('social/naver/', naver_auth, name='naver_auth'),
 ]
