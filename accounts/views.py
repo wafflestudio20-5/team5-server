195104b4ffb0acb296aaeebe6373fa61ab80002b
incipienstation <jinanjeong@snu.ac.kr>
2023-01-16 00:13:31 +0900

    fix: 프로필뷰 분리

--- accounts/views.py
+++ accounts/views.py
@@ -3,19 +3,21 @@ from django.contrib.auth.models import update_last_login
 from django.http import JsonResponse
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.models import CustomUser
 from accounts.serializers import CustomUserDetailsSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def google_auth(request):
     access_token = request.GET.get('token', None)
+    if access_token is None:
+        return JsonResponse({'err_msg': 'query parameter "token" needed'}, status=status.HTTP_400_BAD_REQUEST)
+
     user_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
     user_req_status = user_req.status_code
-
     if user_req_status != 200:
         return JsonResponse({'err_msg': 'failed to get user information'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -28,10 +30,12 @@ def google_auth(request):
 @permission_classes((permissions.AllowAny,))
 def naver_auth(request):
     access_token = request.GET.get('token', None)
+    if access_token is None:
+        return JsonResponse({'err_msg': 'query parameter "token" needed'}, status=status.HTTP_400_BAD_REQUEST)
+
     user_req = requests.get(url="https://openapi.naver.com/v1/nid/me",
                             headers={"Authorization": f"Bearer {access_token}"})
     user_req_status = user_req.status_code
-
     if user_req_status != 200:
         return JsonResponse({'err_msg': 'failed to get user information'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -52,8 +56,7 @@ def _auth(email):
         })
 
     except CustomUser.DoesNotExist:
-        CustomUser(email=email).save()
-        user = CustomUser.objects.get(email=email)
+        user = CustomUser.objects.create(email=email)
         tokens = _generate_tokens(user)
         _login(user)
         return JsonResponse({
