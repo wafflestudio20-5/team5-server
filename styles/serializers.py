53075732ce3ed18ffb213767fe604c2d0e11edb6
Jinan Jeong <jinanjeong@snu.ac.kr>
2023-01-17 17:16:04 +0900

    feat: PostView

--- styles/serializers.py
+++ styles/serializers.py
@@ -24,44 +24,6 @@ class NestedProfileSerializer(serializers.ModelSerializer):
         return {**representation, 'following': following}
 
 
-class FollowerSerializer(serializers.ModelSerializer):
-    from_profile = NestedProfileSerializer(read_only=True)
-
-    class Meta:
-        model = Follow
-        fields = ['from_profile', 'created_at']
-        read_only_fields = ['from_profile', 'created_at']
-
-    def to_representation(self, instance: Follow):
-        representation = super().to_representation(instance)
-        current_user: CustomUser = self.context['current_user']
-        if current_user.id == instance.from_profile_id:
-            return {**representation, 'following': None}
-
-        following = Follow.objects.filter(from_profile=current_user.id,
-                                          to_profile=representation.get('from_profile')['user_id']).exists()
-        return {**representation, 'following': following}
-
-
-class FollowingSerializer(serializers.ModelSerializer):
-    to_profile = NestedProfileSerializer(read_only=True)
-
-    class Meta:
-        model = Follow
-        fields = ['to_profile', 'created_at']
-        read_only_fields = ['to_profile', 'created_at']
-
-    def to_representation(self, instance: Follow):
-        representation = super().to_representation(instance)
-        current_user: CustomUser = self.context['current_user']
-        if current_user.id == instance.to_profile_id:
-            return {**representation, 'following': None}
-
-        following = Follow.objects.filter(from_profile=current_user.id,
-                                          to_profile=representation.get('to_profile')['user_id']).exists()
-        return {**representation, 'following': following}
-
-
 class ProfileSerializer(serializers.ModelSerializer):
     user_id = serializers.PrimaryKeyRelatedField(read_only=True)
     num_followers = serializers.SerializerMethodField()
@@ -87,26 +49,43 @@ class ProfileSerializer(serializers.ModelSerializer):
         if current_user.id == instance.user_id:
             return {**representation, 'following': None}
 
-        following = Follow.objects.filter(from_profile=current_user.id, to_profile=instance.user_id).exists()
+        following = Follow.objects.filter(from_profile_id=current_user.id, to_profile_id=instance.user_id).exists()
         return {**representation, 'following': following}
 
 
+class FollowerSerializer(serializers.ModelSerializer):
+    from_profile = NestedProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = ['from_profile', 'created_at']
+        read_only_fields = ['from_profile', 'created_at']
+
+
+class FollowingSerializer(serializers.ModelSerializer):
+    to_profile = NestedProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = ['to_profile', 'created_at']
+        read_only_fields = ['to_profile', 'created_at']
+
+
 class PostSerializer(serializers.ModelSerializer):
-    post_id = serializers.PrimaryKeyRelatedField(read_only=True)
     created_by = NestedProfileSerializer(read_only=True)
     num_comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['post_id', 'content', 'created_by', 'created_at', 'num_comments']
-        read_only_fields = ['post_id', 'created_by', 'created_at', 'num_comments']
+        fields = ['id', 'content', 'created_by', 'created_at', 'num_comments']
+        read_only_fields = ['id', 'created_by', 'created_at', 'num_comments']
 
     def get_num_comments(self, obj: Post):
         return obj.comments.count() + obj.replies.count()
 
     def create(self, validated_data):
         current_user: CustomUser = self.context['current_user']
-        instance = Post.objects.create({**validated_data, 'created_by_id': current_user.id})
+        instance = Post.objects.create(**validated_data, created_by_id=current_user.id)
         return instance
 
 
--- styles/urls.py
+++ styles/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from styles import views
 
 urlpatterns = [
+    path('profiles/', views.ProfileListAPIView.as_view()),
     path('profiles/<int:user_id>/', views.ProfileRetrieveUpdateAPIView.as_view()),
     path('profiles/<int:user_id>/followers/', views.FollowerListAPIView.as_view()),
     path('profiles/<int:user_id>/followings/', views.FollowingListAPIView.as_view()),
     path('profiles/<int:user_id>/follow/', views.follow, name='follow/unfollow'),
     path('posts/', views.PostListCreateAPIView.as_view()),
-    path('posts/<int:post_id>/', views.PostRetrieveUpdateDestroyAPIView.as_view()),
-    path('posts/<int:post_id>/comments/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
-    path('comments/<int:comment_id>/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
-    path('comments/<int:comment_id>/replies/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
-    path('replies/<int:reply_id>/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
+    path('posts/<pk>/', views.PostRetrieveUpdateDestroyAPIView.as_view()),
+    path('posts/<pk>/comments/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
+    path('comments/<pk>/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
+    path('comments/<pk>/replies/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
+    path('replies/<pk>/', views.CommentListCreateAPIView.as_view(), name='post-comment-list'),
 ]
