f97bb486eba2ea7b5e3b8f4383b2c3556b4af6af
incipienstation <jinanjeong@snu.ac.kr>
2023-01-18 10:46:53 +0900

    feat: PostView

--- styles/permissions.py
+++ styles/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-from styles.models import Profile
+from styles.models import Profile, Post
 
 
 class IsProfileOwnerOrReadOnly(permissions.BasePermission):
@@ -8,4 +8,14 @@ class IsProfileOwnerOrReadOnly(permissions.BasePermission):
         return bool(
             request.method in permissions.SAFE_METHODS or
             request.user and request.user.is_authenticated and
-            request.user == obj.user)
+            request.user == obj.user
+        )
+
+
+class IsPostWriterOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj: Post):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and request.user.is_authenticated and
+            request.user == obj.created_by.user
+        )
--- styles/serializers.py
+++ styles/serializers.py
@@ -1,61 +1,38 @@
 from rest_framework import serializers
 
 from accounts.models import CustomUser
-from styles.models import Follow, Profile
+from styles.models import Follow, Profile, Post, Reply, Comment
 
 
 class NestedProfileSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Profile
-        fields = ['user', 'user_name', 'profile_name', 'img']
+        fields = ['user_id', 'user_name', 'profile_name', 'image']
 
-
-class FollowerSerializer(serializers.ModelSerializer):
-    from_profile = NestedProfileSerializer(read_only=True)
-
-    class Meta:
-        model = Follow
-        fields = ['from_profile', 'created_at']
-        read_only_fields = ['from_profile', 'created_at']
-
-    def to_representation(self, instance: Follow):
+    def to_representation(self, instance: Profile):
         representation = super().to_representation(instance)
         current_user: CustomUser = self.context['current_user']
-        if current_user.id == instance.from_profile_id:
+        if current_user.is_anonymous:
+            return {**representation, 'following': 'login required'}
+
+        if current_user.id == instance.user_id:
             return {**representation, 'following': None}
 
-        following = Follow.objects.filter(from_profile=current_user.id,
-                                          to_profile=representation.get('from_profile')['user']).exists()
-        return {**representation, 'following': following}
-
-
-class FollowingSerializer(serializers.ModelSerializer):
-    to_profile = NestedProfileSerializer(read_only=True)
-
-    class Meta:
-        model = Follow
-        fields = ['to_profile', 'created_at']
-        read_only_fields = ['to_profile', 'created_at']
-
-    def to_representation(self, instance: Follow):
-        representation = super().to_representation(instance)
-        current_user: CustomUser = self.context['current_user']
-        if current_user.id == instance.to_profile_id:
-            return {**representation, 'following': None}
-
-        following = Follow.objects.filter(from_profile=current_user.id,
-                                          to_profile=representation.get('to_profile')['user']).exists()
+        following = Follow.objects.filter(from_profile_id=current_user.id, to_profile_id=instance.user_id).exists()
         return {**representation, 'following': following}
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(read_only=True)
     num_followers = serializers.SerializerMethodField()
     num_followings = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ['user', 'user_name', 'profile_name', 'introduction', 'img', 'num_followers', 'num_followings']
-        read_only_fields = ['user', 'num_followers', 'num_followings']
+        fields = ['user_id', 'user_name', 'profile_name', 'introduction', 'image', 'num_followers', 'num_followings']
+        read_only_fields = ['user_id', 'num_followers', 'num_followings']
 
     def get_num_followers(self, obj: Profile):
         return obj.followers.count()
@@ -72,5 +49,62 @@ class ProfileSerializer(serializers.ModelSerializer):
         if current_user.id == instance.user_id:
             return {**representation, 'following': None}
 
-        following = Follow.objects.filter(from_profile=current_user.id, to_profile=instance.user_id).exists()
+        following = Follow.objects.filter(from_profile_id=current_user.id, to_profile_id=instance.user_id).exists()
         return {**representation, 'following': following}
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    from_profile = NestedProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = ['from_profile', 'created_at']
+        read_only_fields = ['from_profile', 'created_at']
+
+
+class FollowingSerializer(serializers.ModelSerializer):
+    to_profile = NestedProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = ['to_profile', 'created_at']
+        read_only_fields = ['to_profile', 'created_at']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    created_by = NestedProfileSerializer(read_only=True)
+    num_comments = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'content', 'created_by', 'created_at', 'num_comments']
+        read_only_fields = ['id', 'created_by', 'created_at', 'num_comments']
+
+    def get_num_comments(self, obj: Post):
+        return obj.comments.count() + obj.replies.count()
+
+    def create(self, validated_data):
+        current_user: CustomUser = self.context['current_user']
+        instance = Post.objects.create(**validated_data, created_by_id=current_user.id)
+        return instance
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    reply_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    created_by = NestedProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Reply
+        fields = ['reply_id', 'content', 'created_by', 'created_at']
+        read_only_fields = ['reply_id', 'content', 'created_by', 'created_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    comment_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    created_by = NestedProfileSerializer(read_only=True)
+    replies = ReplySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['comment_id', 'content', 'created_by', 'created_at', 'replies']
+        read_only_fields = ['comment_id', 'created_by', 'created_at']
