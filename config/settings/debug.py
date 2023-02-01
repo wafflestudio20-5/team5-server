6a544f6b00df6116f8fde7cb1f1d4f0db6a525c5
yoonsuhchung <yoonsuh20@gmail.com>
2023-02-02 03:55:37 +0900

    fix: price filter

--- config/settings/debug.py
+++ config/settings/debug.py
@@ -6,8 +6,19 @@ DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
 DATABASES = config_secret_debug['django']['database']
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 # WSGI application
 WSGI_APPLICATION = 'config.wsgi.debug.application'
-
 
 AWS_STORAGE_BUCKET_NAME = 'kream-waffle-debug-bucket'
